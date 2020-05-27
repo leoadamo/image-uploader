98e0679bd4ac7d899a098feeb4f4b176e09a54ac
Leonardo Adamoli <leonardo.adamoli@prodigious.com>
2020-05-26 21:46:24 -0300

    communicating across vuex modules

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import qs from 'qs';
 
 export default {
@@ -10,5 +11,13 @@ export default {
     window.location = `${
       process.env.VUE_APP_API_URL
     }/oauth2/authorize?${qs.stringify(queryString)}`;
+  },
+  fetchImages(token) {
+    return axios.get(
+      `${process.env.VUE_APP_API_URL}/3/account/me/images`,
+      {
+        headers: { Authorization: `Bearer ${token}` }
+      }
+    );
   }
 };
