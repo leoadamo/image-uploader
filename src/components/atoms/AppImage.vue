841618adf180211fff1dd707524bd333576062e5
Leonardo Adamoli <leonardo.adamoli@prodigious.com>
2020-05-27 23:46:37 -0300

    listing images from api

---
+++ src/components/atoms/AppImage.vue
@@ -0,0 +1,25 @@
+<template>
+  <img
+    class="app-image"
+    :src="image.link"
+    :alt="image.name"
+  />
+</template>
+
+<script>
+export default {
+  name: 'AppImage',
+  props: {
+    image: {
+      type: Object
+    }
+  }
+};
+</script>
+
+<style scoped>
+.app-image {
+  display: block;
+  width: 100%;
+}
+</style>
--- src/components/organisms/ImageList.vue
+++ src/components/organisms/ImageList.vue
@@ -1,11 +1,48 @@
 <template>
-  <div>
-    Images List
-  </div>
+  <ul class="image-list">
+    <li
+      class="image-item"
+      v-for="image in allImages"
+      :key="image.id"
+    >
+      <AppImage :image="image" />
+    </li>
+  </ul>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+import AppImage from '@/components/atoms/AppImage';
+
 export default {
-  name: 'ImageList'
+  name: 'ImageList',
+  components: {
+    AppImage
+  },
+  computed: {
+    ...mapGetters(['allImages'])
+  },
+  methods: {
+    ...mapActions(['fetchImages'])
+  },
+  created() {
+    this.fetchImages();
+  }
 };
 </script>
+
+<style scoped>
+.image-list {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  margin: 0 15px;
+  padding: 0;
+}
+
+.image-item {
+  display: flex;
+  align-items: stretch;
+  list-style: none;
+}
+</style>
